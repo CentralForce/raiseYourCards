0f5b7b6398dfd510bbac72e6fab950f7b643f09c
CentralForce <42040823+CentralForce@users.noreply.github.com>
2018-08-08 18:13:03 +0200

    Update config.js
    Config example

--- server/config.js
+++ server/config.js
@@ -1,6 +1,29 @@
-let config = {
-  
+module.exports = {
+  sections: 
+  [
+    {
+      value: NaN,
+      content: [{
+        card: [
+          {
+            value: 1,
+            text: 'Insert Text',
+            class: ["custom-card"]
+          }
+        ]
+      }]
+    },
+    {
+      value: NaN,
+      content: [{
+        card: [
+          {
+            value: 1,
+            text: 'Insert Text',
+            class: ["custom-card"]
+          }
+        ]
+      }]
+    }
+  ]
 }
-
-module.exports = config
-
