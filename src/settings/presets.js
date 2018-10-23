985e1837d32c050d04d1245c4a96785a250e4a83
CentralForce <42040823+CentralForce@users.noreply.github.com>
2018-10-23 21:11:42 +0200

    Create presets.js

---
+++ src/settings/presets.js
@@ -0,0 +1,52 @@
+module.exports = {
+  presets: [
+    {
+      name: "Preset0",
+      rows: [
+        {
+          average: true,
+          cards: [
+            {
+              name: "1 Hour",
+              value: 1
+            },
+            {
+              name: "2 Hours",
+              value: 2
+            }
+          ]
+        },
+        {
+          cards: [
+            {
+              name: "Low",
+              value: 1
+            },
+            {
+              name: "High",
+              value: 2
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "Preset1",
+      rows: [
+        {
+          cards: [
+            {
+              name: "Low",
+              value: 1
+            },
+            {
+              name: "High",
+              value: 2
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
